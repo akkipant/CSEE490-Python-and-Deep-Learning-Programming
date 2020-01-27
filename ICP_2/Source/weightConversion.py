b217cfd8b245180411dbfd34b1b32b862dc0e444
akkipant <44404812+akkipant@users.noreply.github.com>
2020-01-27 16:31:57 -0600

    Initial Commit

--- ICP_2/Source/weightConversion.py
+++ ICP_2/Source/weightConversion.py
@@ -2,7 +2,7 @@ N = int(input("Enter Number of students : "))
 lbsWeight = list()
 kgWeight = list()
 for x in range(N):
-    lbsWeight.append(int(input("Enter Weight for student " + str(x) + " : ")))
+    lbsWeight.append(int(input("Enter Weight for student " + str(x+1) + " : ")))
 
 for x in lbsWeight:
     kgWeight.append(x * 0.454)
