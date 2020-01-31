b213d803ea5f0398ab40d5915015c4fbf9ee7477
akkipant <44404812+akkipant@users.noreply.github.com>
2020-01-31 12:13:03 -0600

    Final Commit

--- ICP_2/Source/strAlternative.py
+++ ICP_2/Source/strAlternative.py
@@ -1,5 +1,13 @@
 def string_Alternative(Str):  # Function Definition
-    return Str[::2]     # Return alternative characters of string
+    #return Str[::2]     # Return alternative characters of string
+    newStr = ''
+    count = 0
+    for x in Str:
+        count += 1
+        if count%2:
+            newStr += x
+
+    return newStr
 
 
 if __name__ == '__main__':
